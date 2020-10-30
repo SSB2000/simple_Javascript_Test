22243fcb3d1352c78b04ef1304dbcd6f5f347f2c
SSB2000 <mshubhambohra@gmail.com>
2020-10-30 22:18:06 +0530

    first commit

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function( [a, b, c, d], del) {
+    let arr = [a, b, c, d];
+    let i = arr.indexOf(del);
+    let arra = arr.splice(i,1);
+    return arra;
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    let string = "";
+    let i = 0;
+    while(i >= 0 && i < n) {
+        string = string + str;
+        i++;
+    }
+    if(n < 0) {
+        string = "ERROR";
+    }
+    return string;
 }
-
 module.exports = repeatString
